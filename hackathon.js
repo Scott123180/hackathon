b641d27065a3df4539a7ccdf01349968fee85e39
Scott123180 <Scott123180@gmail.com>
2014-11-22 14:07:53 -0500

    duck this we got the api key hopefully

--- hackathon.js
+++ hackathon.js
@@ -1,8 +1,11 @@
 // JavaScript source code
 
+var apiKey = "27f7d998-e81b-464a-904f-b565c3a33f41"
+
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://api.idolondemand.com/1/api/sync/recognizeimages/v1", false);
 xhr.send();
 
-console.log(xhr.status);
 
+console.log(xhr.status);
+console.log(xhr.statusText);
