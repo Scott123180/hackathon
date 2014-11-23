3991cec672c997310f88f95e6a797fcbf8a95cb4
Scott123180 <Scott123180@gmail.com>
2014-11-23 03:50:18 -0500

    init of array with building information

---
+++ building-references.js
@@ -0,0 +1,5 @@
+// JavaScript Document
+
+var names = [ "", "Byrne House", "James A. Cannavino Library", "Champagnat Hall", "Our Lady Seat of Wisdom Chapel", "Cornell Boathouse", "Donnelly Hall", "Margaret M. and Charles H. Dyson Center", "Fern Tor", "Fontaine  Annex", "Fontaine  Hall", "Foy Townhouses", "Fulton Street Townhouses", "Lower Fulton Townhouses", "Gartland Appartments", "Greystone Hall", "Hancock Center", "Kieran Gatehouse", "Kirk House", "Leo Hall", "Longview Park", "Lowell Thomas Communications Center", "Marian Hall", "Marist Boathouse", "James J. McCann Recreational Center", "Mid-Rise Hall", "St. Ann’s Hermitage", "St. Peter’s", "Sheahan Hall", "Steel Plant Art Studios and Gallery", "Student Center/Rotunda", "Tennis Pavilion", "Tenney Stadium", "Lower Townhouses", "Lower West Cedar Townhouses", "Upper West Cedar Townhouses"];
+
+console.log(names[35]);
