9bdd1287748a41c42a886da9a0cfe8206c1eda71
Scott123180 <Scott123180@gmail.com>
2014-11-23 05:24:32 -0500

    finalized building arrays

--- building-references.js
+++ building-references.js
@@ -10,6 +10,7 @@ var descriptions = ["",
 "This building contains the Academic Learning Center, Academic Technology, Center for Career Services, Center for Multicultural Affairs", 
 "This building contains Freshman Residence", 
 "This building is where religious services take place",
+"This building contains storage for boats as well as a studio upstairs for many events.",
 "This building contains: Academic Advising, Business and Financial Affairs, Computer Center/Information Technology, Computer Store, Digital Publications Center, Donnelly Coffee Shop, Fashion Program, FoxNet, Institutional Research and Planning, Office of the Registrar, ResNet, Safety and Security, Special Services, and Student Financial Services",
 "This building contains the Dyson Coffee Shop, School of Management, School of Social & Behavioral Sciences, and Teacher Education Program ",
 "This area contains Arboretum and Nature Trails",
@@ -27,7 +28,37 @@ var descriptions = ["",
 "This hosts the Riverfront Recreational Area",
 "This newly renovated building contains the Communications Center, School of Communication and the Arts, School of Global &Professional Programs, Media Center, Center for Sports Communication.",
 "This building houses Freshman and Sophomore Residence",
-"This building contains storage for boats as well as a studio upstairs for many events.",
+"This building contains storage for boats.",
 "This building contains the Recreational Center, Athletic Department, Aerobic Center, Center for Sports Medicine, Field House, Fitness Center, Hall of Fame Multimedia Room, Natatorium, and the Varsity Coaches Center",
 "This houses Sophomore Residence",
 "This building is used for ROTC programs",
+"This building contains Upward Bound",
+"This building houses Freshman Residences",
+"This building contains an Art Gallery, Digital Media, and a Fine Arts Program",
+"This building contains the Admission Visitor Center, Campus Bookstore, Dining Services/Cabaret, Health Services, Music Department, Nelly Goletti Theatre, Post Office, Student Activities, Student Affairs, Student Government Association, and Student Housing Office",
+"This area is used in the sport of tennis.",
+"This field and bleachers hosts games for all of the major field-based sports games and practices.",
+"These house Upperclass Residence.",
+"These house Upperclass Residence.",
+"These house Upperclass Residence.",
+];
+
+// it works
+//console.log(descriptions[32]);
+//console.log(descriptions[35]);
+
+var links = ['','http://library.marist.edu/archives/MHP/residential/byrnehouse.html','http://library.marist.edu/about/JAC_history.html','http://www.marist.edu/housing/champagn.html',
+'http://library.marist.edu/archives/MHP/chapel/chapel.html','http://www.goredfoxes.com/ViewArticle.dbml?ATCLID=205240115','http://library.marist.edu/archives/MHP/donnellyhall/donnelly.html',
+'http://library.marist.edu/archives/MHP/Marist%20Buildings/Dyson.html','http://library.marist.edu/archives/MHP/Marist%20Buildings/Fern_Tor.html','',
+'http://library.marist.edu/archives/MHP/fontainehall/fontainehomepage.html','http://www.marist.edu/housing/foytownh.html','http://www.marist.edu/housing/fulton.html',
+'http://www.marist.edu/housing/newfulton.html','http://www.marist.edu/housing/gartland.html','http://library.marist.edu/archives/MHP/greystone/greystone.html',
+'http://www.universitybusiness.com/article/marist-colleges-hancock-center','http://library.marist.edu/archives/MHP/kierangatehouse/kieran.html',
+'http://library.marist.edu/archives/MHP/residential/kirkhouse.html','http://www.marist.edu/housing/leo.html','http://www.goredfoxes.com/ViewArticle.dbml?ATCLID=205241066',
+'http://library.marist.edu/archives/MHP/Marist%20Buildings/Lowell_Thomas.html','http://www.marist.edu/housing/marian.html','http://library.marist.edu/archives/MHP/residential/boathouse.html',
+'http://library.marist.edu/archives/MHP/Marist%20Buildings/McCann.html','http://www.marist.edu/housing/midrise.html','','http://library.marist.edu/archives/MHP/st.peters/stpeters.html',
+'http://www.marist.edu/housing/sheahan.html','http://chrissy51820.tripod.com/index.html','http://www.marist.edu/studentlife/studentcenter.html',
+'http://www.goredfoxes.com/ViewArticle.dbml?ATCLID=205241114','http://www.goredfoxes.com/ViewArticle.dbml?ATCLID=205239935','','http://www.marist.edu/housing/lwcedar.html',
+'http://www.marist.edu/housing/uwcedar.html'];
+
+// it works
+//console.log(links[35]);
